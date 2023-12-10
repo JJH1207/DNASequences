6ed230f3c1db60d79f04f5f72f8f2e92984d3884
Hunt <huntj9@vdi-scclab-015.lancs.local>
2023-12-10 14:05:28 +0000

    currently matches possibility of pattern character to one buf character

--- main.js
+++ main.js
@@ -2,11 +2,47 @@
 
 const testlib = require('./testlib.js');
 
+let possiblities = {
+    A : ["A"],
+    G : ["G"],
+    T : ["T"],
+    C : ["C"],
+    R : ["G", "A"],
+    Y : ["T", "C"],
+    K : ["G", "T"],
+    M : ["A", "C"], 
+    S : ["G", "C"],
+    W : ["A", "T"],
+    B : ["G", "T", "C"],
+    D : ["G", "A", "T"],
+    H : ["A", "C", "T"],
+    V : ["G", "C", "A"],
+    N : ["A", "G", "C", "T"]
+}
+
 let current_input = {
     data_count : {}, // stores count for each pattern e.g. {"AA" : 2, "CCA": 6}
     pattern_buff : {}, // stores the pattern sized buffer e.g. { "AA": [], "CCA": [] }
+    possible : possiblities,
     position : 0,
-    updateBufs : function(data) {
+    init : function(patterns) {
+        // creates an array of counts for each pattern
+    this.data_count = patterns.reduce((a, x) => {
+        a[x] = 0;
+        return a;
+    }, 
+    {}
+    );
+
+    // creates buffers for each pattern
+    this.pattern_buff = patterns.reduce((a, x) => {
+        a[x] = [];
+        return a;
+    },
+    {}
+    );
+    },
+    updateBufs : function(data) { // adds new data to each buffer appropriately
         Object.keys(this.pattern_buff).forEach((key) => {
     
             if(key.length > this.pattern_buff[key].length) // buffer size is not exceeded
@@ -16,36 +52,53 @@ let current_input = {
                 this.pattern_buff[key].push(data); // add data to the back
             }
             // check for match
+            this.isMatch(key)
             if(key === this.pattern_buff[key].join('')){
                 this.data_count[key]++;
                 testlib.foundMatch(this.pattern_buff[key], this.position-this.pattern_buff[key].length);
             }
         });
     },
+    isMatch: function(key) {
+        // current pattern in characters
+        let current_pattern = key.split("");
+        let current_buf = this.pattern_buff[key];
+        if(current_buf.length !== current_pattern.length)
+            return false;
+
+        let index = 0;
+        current_buf.reduce((flag) => { // looks at each character in the buffer
+            let buf_char = current_buf[index];
+            let pat_char = current_pattern[index];
+            let char_match = possiblities[buf_char].reduce((matched) => { // compares all possibilities of buffer character to possibilities of pattern character
+                let found = possiblities[pat_char].indexOf(buf_char);
+                //console.log(possiblities[pat_char], buf_char, found);
+                if(found !== -1)
+                    return true;
+                else    
+                    return matched;
+            },
+            false  // character match assumes false unless we find a matching possibility
+            )
+            if(char_match === false) // if one character match is false the there is no match in general
+                return false;
+            else
+                return flag;
+            },
+            true
+            )
+
+    },
     resetBufs : function() {
-        Object.values(this.pattern_buff).forEach((value) => value.length = 0);
-        Object.values(this.data_count).forEach((value) => value = 0);
+        Object.values(this.pattern_buff).forEach((value) => value.length = 0); // buffer is emptied
+        //Object.values(this.data_count).forEach((value) => value = 0); // counts reset to 0
         this.position = 0;
     }
 };
 
 testlib.on( 'ready', function( patterns ) {
-    // creates an array of counts for each pattern
-    current_input.data_count = patterns.reduce((a, x) => {
-        a[x] = 0;
-        return a;
-    }, 
-    {}
-    );
-
-    // creates buffers for each pattern
-    current_input.pattern_buff = patterns.reduce((a, x) => {
-        a[x] = [];
-        return a;
-    },
-    {}
-    );
-
+    
+    current_input.init(patterns);
 	console.log( "Patterns:", patterns );
 	testlib.runTests();
 } );
@@ -60,15 +113,14 @@ testlib.on( 'data', function( data ) {
 
 testlib.on( 'reset', function () {
 
-    testlib.frequencyTable(current_input.data_count);
     current_input.resetBufs();
     
 } );
 
 testlib.on( 'end', function() {
-
-    console.log( "End" );
+    
+    testlib.frequencyTable(current_input.data_count);
 
 } );
 
-testlib.setup(2); // Runs test 1 (task1.data and task1.seq)
+testlib.setup(3); // Runs test 1 (task1.data and task1.seq)
